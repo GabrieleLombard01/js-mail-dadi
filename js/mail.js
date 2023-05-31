152e5d37887e1373c690a9b98a489cfc21e6203a
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-05-31 22:42:28 +0200

    cicle added

--- js/mail.js
+++ js/mail.js
@@ -4,4 +4,23 @@ console.log('JS OK')
 const mailInput = document.getElementById('email');
 const button = document.getElementById('button');
 
+//creo la lista arey
+const emailList = ['gabbo.lombardo2001@gmail.com', 'valentino.rossi@gmail.com','luca.sape@gmail.it', 'ciccio.bomba@gmail.it', 'gabriele.lombardo@gmail.com'];
 
+button.addEventListener('click', function(){
+    
+    const userEmail = mailInput.value.trim();
+
+     console.log(userEmail)
+
+    // creo un ciclo x mail gia presente
+    for (let i = 0; i < emailList.length; i++ ){
+        
+        if (emailList.includes(userEmail)){
+            console.log('Email già presente');
+        } else {
+            emailList.push(userEmail);
+            console.log('Email aggiunta');
+        }
+    } 
+});
