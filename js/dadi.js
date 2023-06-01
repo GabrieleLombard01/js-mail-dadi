952762135b6a71e87f5e1f6b813e6bf8b9aa9533
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-06-01 09:47:16 +0200

    dadi fixed

--- js/dadi.js
+++ js/dadi.js
@@ -4,6 +4,8 @@ console.log('JS OK')
 /* Generare un numero random da 1 a 6 per l'utente e per la CPU,
    stabilire il vincitore, in base a chi fa il punteggio più alto */
 
+const displayElement = document.getElementById('display');
+
 const playerNumber = Math.floor(Math.random()* 6) + 1;
 const cpuNumber = Math.floor(Math.random()* 6) + 1;
 
@@ -16,4 +18,9 @@ if (playerNumber > cpuNumber) {
 } else if (playerNumber < cpuNumber) { 
     result = 'Ha vinto la CPU';
 }
-console.log(result);|
+//console.log(result);|
+
+displayElement.innerHTML = `
+<p>Tiro Giocatore: ${playerNumber}</p> 
+<p>Tiro CPU: ${cpuNumber}</p> 
+<p><strong>${result}</strong></p> `
