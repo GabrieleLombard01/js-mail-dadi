378340ceef2cda79c70ad926ff7bc83c43c3038d
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-06-01 09:37:37 +0200

    const dadi

--- js/dadi.js
+++ js/dadi.js
@@ -3,3 +3,6 @@ console.log('JS OK')
 
 /* Generare un numero random da 1 a 6 per l'utente e per la CPU,
    stabilire il vincitore, in base a chi fa il punteggio più alto */
+
+const playerNumber = Math.floor(Math.random()* 6) + 1;
+const cpuNumber = Math.floor(Math.random()* 6) + 1;
